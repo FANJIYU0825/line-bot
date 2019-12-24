d4c19d35c921871011b805842746fef8c08ff3b0
FANJIYU0825 <55987247+FANJIYU0825@users.noreply.github.com>
2019-12-24 23:39:28 +0800

    add code

---
+++ sever.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('8G0CdpifvSTnzWJRoK8sxDHEc67YxlLgPiaLLwk/rXO2s4+fyopQh48ioZBQa1g2Jm23JfCcqbz6XqFM/lhk9VaMJVwKHgKN0/C8J0WObTnhYHEn7T9c+0PZeh0Nppfl1s9Kg5/QyvTilXlkciH8xQdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('Ub7373054fdb8cc5382e0bcf7e9fd34a6')
+
+@app.route('/')
+def index():
+    return'Welcome to Line bot'
+
+@app.route("/callback",methods = ['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text =True)
+
+    try:
+        handler.handle(body, signature)
+
+    except InvalidSignatureError:
+        abort(400)
+    
+    return 'Ok!!!'
+
+@handler.default()
+def default(event):
+    print('捕捉事件:',event )
+
+if __name__ =="__main__":
+    app.run(debug= True,host = "0.0.0.0",port=80)
