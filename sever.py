c299cd523792bf2b2493d2b70c3db6de849d5f7b
FANJIYU0825 <55987247+FANJIYU0825@users.noreply.github.com>
2019-12-28 15:45:07 +0800

    add

--- sever.py
+++ sever.py
@@ -33,4 +33,4 @@ def default(event):
     print('捕捉事件:',event )
 
 if __name__ =="__main__":
-    app.run(debug= True,host = "0.0.0.0",port=80)
+    app.run(debug= True,host = "127.0.0.1",port=5000)
