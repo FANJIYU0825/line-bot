2b176cd4f886ef46eced00986cdc8e9a130b85d5
FANJIYU0825 <55987247+FANJIYU0825@users.noreply.github.com>
2019-12-28 15:41:53 +0800

    add

--- sever.py
+++ sever.py
@@ -1,15 +1,19 @@
-from flask import Flask, request, abort
+from flask import (Flask, request , abort,render_template)
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
+
 
 app = Flask(__name__)
 
 line_bot_api = LineBotApi('8G0CdpifvSTnzWJRoK8sxDHEc67YxlLgPiaLLwk/rXO2s4+fyopQh48ioZBQa1g2Jm23JfCcqbz6XqFM/lhk9VaMJVwKHgKN0/C8J0WObTnhYHEn7T9c+0PZeh0Nppfl1s9Kg5/QyvTilXlkciH8xQdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('Ub7373054fdb8cc5382e0bcf7e9fd34a6')
+handler = WebhookHandler('b584cdb1b112abe097e5a0da99903682') #b584cdb1b112abe097e5a0da99903682
+
+
+
 
 @app.route('/')
 def index():
-    return'Welcome to Line bot'
+    return render_template("index.html")
 
 @app.route("/callback",methods = ['POST'])
 def callback():
